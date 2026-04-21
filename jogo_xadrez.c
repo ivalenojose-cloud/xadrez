2219976581393bfbe6e55574489987fdc3f2bc24
ivalenojose-cloud <ivalenojose@gmail.com>
2026-04-21 13:16:16 -0300

    Add files via upload

---
+++ jogo_xadrez.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // Número de casas para cada peça
+    int casasTorre = 5;
+    int casasBispo = 5;
+    int casasRainha = 8;
+
+    
+    // Torre: usando FOR
+    printf("Movimento da Torre:\n");
+    for (int i = 1; i <= casasTorre; i++) {
+        printf("Direita\n");
+    }
+
+    // Bispo: usando WHILE
+    printf("\nMovimento do Bispo:\n");
+    int j = 1;
+    while (j <= casasBispo) {
+        printf("Cima, Direita\n");
+        j++;
+    }
+
+    // Rainha: usando DO-WHILE
+    printf("\nMovimento da Rainha:\n");
+    int k = 1;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k <= casasRainha);
+
+    return 0;
+}
